dc5be2d8ec729b85ffcddbcb544202d53870d0be
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2021-11-07 15:34:23 +0000

    cleaned

--- Main.py
+++ Main.py
@@ -1,16 +1,15 @@
+import random 
+
+# import tkinter as tk
+import tkinter as tk
 
 def photos():
   photoArray = ["AligatorLoki.jpg","BaldNonce.jpg", "BorisJohnson.jpeg", "Dadosaur.png",  "DannyDorito.jpg", "Doll.jpg", "Drip_Goku.jpg", "freecuthbert.jpg", "Halo3Rat.jpg", "hilda.jpeg", "IceAgeBaby.jpg", "imthetrashman.png", "johnxina.jpg", "keanu.jpg", "KevinMug.jpg", "KrisJongUn.png", "ladydimitrescu.png", "lobsterwoman.jpg", "PsychicFriendFredbear.jpg" , "RapperMario.jpg" , "SlavCat.jpg" , "SquirtGunPriest.jpg" , "StudyGirl.jpg" , "the_wock.jpg" , "wurmple.png"]
 
-# import tkinter as tk
-import tkinter as tk
 # create an instance of the tk.Tk class - this creates the window. 
 root = tk.Tk()
 # setting tkinter size 
 root.geometry('600x400+50+50')
-
-
-import random 
 
 with open ("6K adverbs.txt", "r") as adverbs:
     adverbList = adverbs.readlines()
