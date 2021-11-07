268f4e9b722f71dba3bb7d3b611857f634859dc3
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2021-11-07 16:03:55 +0000

    upadted mian.py

--- Main.py
+++ Main.py
@@ -3,8 +3,12 @@ import random
 # import tkinter as tk
 import tkinter as tk
 
-def photos():
-  photoArray = ["AligatorLoki.jpg","BaldNonce.jpg", "BorisJohnson.jpeg", "Dadosaur.png",  "DannyDorito.jpg", "Doll.jpg", "Drip_Goku.jpg", "freecuthbert.jpg", "Halo3Rat.jpg", "hilda.jpeg", "IceAgeBaby.jpg", "imthetrashman.png", "johnxina.jpg", "keanu.jpg", "KevinMug.jpg", "KrisJongUn.png", "ladydimitrescu.png", "lobsterwoman.jpg", "PsychicFriendFredbear.jpg" , "RapperMario.jpg" , "SlavCat.jpg" , "SquirtGunPriest.jpg" , "StudyGirl.jpg" , "the_wock.jpg" , "wurmple.png"]
+
+# import image library
+
+from PIL import ImageTk,Image
+
+photoArray = ["AligatorLoki.jpg","BaldNonce.jpg", "BorisJohnson.jpeg", "Dadosaur.png",  "DannyDorito.jpg", "Doll.jpg", "Drip_Goku.jpg", "freecuthbert.jpg", "Halo3Rat.jpg", "hilda.jpeg", "IceAgeBaby.jpg", "imthetrashman.png", "johnxina.jpg", "keanu.jpg", "KevinMug.jpg", "KrisJongUn.png", "ladydimitrescu.png", "lobsterwoman.jpg", "PsychicFriendFredbear.jpg" , "RapperMario.jpg" , "SlavCat.jpg" , "SquirtGunPriest.jpg" , "StudyGirl.jpg" , "the_wock.jpg" , "wurmple.png"]
 
 # create an instance of the tk.Tk class - this creates the window. 
 root = tk.Tk()
@@ -27,11 +31,31 @@ with open ("91K nouns.txt", "r") as nouns:
     nounsList = nouns.readlines()
 nouns.close()
 
-sentence = (" {} {} {} {} {}".format("PlaceHolder", verbsList[random.randint(0,len(verbsList))], nounsList[random.randint(0,len(nounsList))],\
+sentence = (" {} {} {} {} {}".format("", verbsList[random.randint(0,len(verbsList))], nounsList[random.randint(0,len(nounsList))],\
     adjectiveList[random.randint(0,len(adjectiveList))], adverbList[random.randint(0,len(adverbList))]))
 sentence = sentence.strip()
 
-
 print (sentence)
 
+x = len(photoArray)
+
+
+
+# getting image 
+img = ImageTk.PhotoImage(Image.open(photoArray[random.randint(0,x)]))
+# adding sentence 
+l = tk.Label(root, text = sentence)
+l.pack()
+
+root = Tk()
+
+# Create a photoimage object of the image in the path
+image1 = Image.open("<path/image_name>")
+test = ImageTk.PhotoImage(image1)
+
+label1 = tkinter.Label(image=test)
+label1.image = test
+
+# Position image
+label1.place(x=<x_coordinate>, y=<y_coordinate>)
 root.mainloop()
