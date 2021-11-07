30996ad790ddd5e1851f2a2ff6884fe3794d91f5
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2021-11-07 15:33:56 +0000

    merged sam's code

--- Main.py
+++ Main.py
@@ -10,4 +10,29 @@ root = tk.Tk()
 root.geometry('600x400+50+50')
 
 
+import random 
+
+with open ("6K adverbs.txt", "r") as adverbs:
+    adverbList = adverbs.readlines()
+adverbs.close()
+
+with open ("28K adjectives.txt", "r") as adjectives:
+    adjectiveList = adjectives.readlines()
+adjectives.close()
+
+with open ("31K verbs.txt", "r") as verbs:
+    verbsList = verbs.readlines()
+verbs.close()
+
+with open ("91K nouns.txt", "r") as nouns:
+    nounsList = nouns.readlines()
+nouns.close()
+
+sentence = (" {} {} {} {} {}".format("PlaceHolder", verbsList[random.randint(0,len(verbsList))], nounsList[random.randint(0,len(nounsList))],\
+    adjectiveList[random.randint(0,len(adjectiveList))], adverbList[random.randint(0,len(adverbList))]))
+sentence = sentence.strip()
+
+
+print (sentence)
+
 root.mainloop()
