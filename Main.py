b8bdbfc74ad39929446404e1511f6ad9cadd7a97
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2021-11-07 15:28:55 +0000

    commit to master

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ def photos():
   photoArray = ["AligatorLoki.jpg","BaldNonce.jpg", "BorisJohnson.jpeg", "Dadosaur.png",  "DannyDorito.jpg", "Doll.jpg", "Drip_Goku.jpg", "freecuthbert.jpg", "Halo3Rat.jpg", "hilda.jpeg", "IceAgeBaby.jpg", "imthetrashman.png", "johnxina.jpg", "keanu.jpg", "KevinMug.jpg", "KrisJongUn.png", "ladydimitrescu.png", "lobsterwoman.jpg"]
 
 
-
 # import tkinter as tk
 import tkinter as tk
 # create an instance of the tk.Tk class - this creates the window. 
