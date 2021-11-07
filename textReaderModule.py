c8e769c8bc3b2fc3e0bb24b01470c069c1791346
Samuel J Orman-Chan <samorman.chan@outlook.com>
2021-11-07 15:13:25 +0000

    working for all txts, but hardcoded for each manually.

--- textReaderModule.py
+++ textReaderModule.py
@@ -1,7 +1,21 @@
 #!
 
+import random
+
 with open ("6K adverbs.txt", "r") as adverbs:
     adverbList = adverbs.readlines()
 adverbs.close()
-for adverbList in adverbList:
-    print (adverbList)
+
+with open ("28K adjectives.txt", "r") as adjectives:
+    adjectiveList = adjectives.readlines()
+adjectives.close()
+
+with open ("31K verbs.txt", "r") as verbs:
+    verbsList = verbs.readlines()
+verbs.close()
+
+with open ("91K nouns.txt", "r") as nouns:
+    nounsList = nouns.readlines()
+nouns.close()
+
+
