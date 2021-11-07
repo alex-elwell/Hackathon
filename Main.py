866f848069fcc7dfe3c09bb28a229f545f5ddea3
Alexander Elwell <alexander.hayden.elwell@gmail.com>
2021-11-07 15:26:18 +0000

    added main.py again

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+main.py 
+
+
 def photos():
   photoArray = ["AligatorLoki.jpg","BaldNonce.jpg", "BorisJohnson.jpeg", "Dadosaur.png",  "DannyDorito.jpg", "Doll.jpg", "Drip_Goku.jpg", "freecuthbert.jpg", "Halo3Rat.jpg", "hilda.jpeg", "IceAgeBaby.jpg", "imthetrashman.png", "johnxina.jpg", "keanu.jpg", "KevinMug.jpg", "KrisJongUn.png", "ladydimitrescu.png", "lobsterwoman.jpg"]
 
